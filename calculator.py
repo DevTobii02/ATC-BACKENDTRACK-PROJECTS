61cea1c0f6823d6258b73261bcde2cdae7cce741
Adebayo Abdullahi <tobiithedev@gmail.com>
2025-04-06 23:40:25 -1200

    Finished

--- calculator.py
+++ calculator.py
@@ -1,15 +1,19 @@
+print("This Is A Basic Calculator Project")
 Number1 = int(input("Enter First Number  : "))
 Number2 = int(input("Enter Second Number :  "))
 Opeartor = input("Enter Your Operator : ")
 if Opeartor == "+":
     Number1 + Number2
-    print(Number1 + Number2) 
+    print("The Result Of Your Operation Is : ", Number1 + Number2) 
 elif Opeartor == "-":
     Number1 - Number2
-    print(Number1 - Number2)
+    print("The Result Of Your Operation  Is : ", Number1 - Number2)
     if Number1 < Number2:
         Number2 - Number1
-        print(Number2 - Number1)
+        print("The Result Of Your Operation Is : ", Number2 - Number1)
 elif Opeartor == "/" : 
     Number1 / Number2
-    print(Number1 / Number2)        
+    print("The Result Of Your Operation Is : ",  Number1 / Number2)
+elif Opeartor == "*":
+    Number1 * Number2
+    print("The Result Of Your Operation Is : ", Number1 * Number2)            
