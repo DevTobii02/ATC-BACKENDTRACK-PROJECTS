8a2321de7ec824f28ae25fe1f8b570a2c85df8a5
Adebayo Abdullahi <tobiithedev@gmail.com>
2025-04-10 01:04:42 -1200

    provides grade  based on input received

--- GradeCalculator.py
+++ GradeCalculator.py
@@ -3,6 +3,12 @@ print("Check Your Score Grade Here")
 Score = float(input("Enter A Score : "))
 if Score >= 70 and Score <= 100:
     print("Your Grade Is A")
+elif Score >= 60 and Score <= 69.9:
+    print("Your Grade Is B")    
+elif Score  >= 50 and Score <= 59.9:
+    print("Your Grade Is C ")
+elif Score >= 45 and Score <= 49.9:
+    print("Your Grade Is E")    
 elif Score >=0 and Score <=39:
     print("Your Grade Is F")  
 else:
