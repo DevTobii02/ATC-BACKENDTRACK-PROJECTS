ef228ad666a1897d21ceaa553eeda58f82c4d809
Adebayo Abdullahi <tobiithedev@gmail.com>
2025-04-10 13:28:31 -1200

    added a while loop that allows the program to keep running until condition is met

--- SimpleLogin.py
+++ SimpleLogin.py
@@ -1,11 +1,19 @@
 #Simple login that stores data per runtime 
-enter_email = input("Enter Your Email : ")
-enter_password =  input("Enter Your Password : ")
+while True:
+    enter_email = input("Enter Your Email: ")
+    enter_password = input("Enter Your Password: ")
+    confirm_email = input("ReEnter Your Email: ")
+    confirm_password = input("ReEnter Your Password: ")
 
-confirm_email = input("ReEnter Your Email : ")
-confirm_password = input("ReEnter Your Password : ")
-
-if enter_email == confirm_email and enter_password == confirm_password:
-    print("Logged In Successfully")
-else:
-    print("Wrong Login Details") 
+    if enter_email == confirm_email and enter_password == confirm_password:
+        print("Logged In Successfully")
+    else:
+        print("Wrong Login Details")     
+        
+    exit_choice = input("Type 'exit' to quit or any key to continue: ")
+    if exit_choice.lower() == 'exit':
+        print("Exiting program...")
+        break
+        
+        
+        
